a471d5ad4bd2ed175512c49b187bf305cf17db41
uoflcoder <tylorcornett@gmail.com>
2018-11-08 14:51:04 -0500

    Farther

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,5 +2,20 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN
 {
+    public boolean checkISBN(String isbn)
+    {
+       int total = 0;
 
+       for(int i = 0; i < isbn.length(); i++)
+       {
+           total = total +   ((10 - i) * Integer.parseInt(Character.toString(isbn.charAt(i))));
+       }
+
+       if(total % 11 == 0)
+       {
+           return true;
+       }
+
+        return false;
+    }
 }
--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -1,12 +1,31 @@
 package com.virtualpairprogrammers.isbntools;
-
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 
 public class ValidateISBNTest
 {
+
     @Test
-    public void test()
+    public void checkAValidISBN()
     {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue("First value",result);
+
+        result = validator.checkISBN("0140177396");
+        assertTrue("Second value", result);
     }
+
+    @Test
+    public void checkAnInvalidISBN()
+    {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
+
+
 }
